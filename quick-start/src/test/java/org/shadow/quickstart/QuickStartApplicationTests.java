2254a879335114917bb0f5b69b93a3289fd031e3
jianjustin <janine.jian.chen@gmail.com>
2020-05-04 16:20:55 +0800

    add module quick-start

---
+++ quick-start/src/test/java/org/shadow/quickstart/QuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shadow.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
